69aedcd1d2763d6cb5ea2999c1b65bf46f0267ca
Enrique Hernandez <enrique_inf@hotmail.com>
2023-06-20 21:38:20 -0600

    Agregar archivos de proyecto.

---
+++ XamarinUdemy/XamarinUdemy/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinUdemy
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+                //do not navigate
+            }
+            else
+            {
+                //navigate
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
