a92b9f9179d7deff0da05fbe06e1787374590b8e
Enrique Hernandez <enrique_inf@hotmail.com>
2023-06-21 18:03:04 -0600

    agregando NewTravelPage

--- XamarinUdemy/XamarinUdemy/NewTravelPage.xaml.cs
+++ XamarinUdemy/XamarinUdemy/NewTravelPage.xaml.cs
@@ -10,16 +10,11 @@ using Xamarin.Forms.Xaml;
 namespace XamarinUdemy
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class HomePage : TabbedPage
+    public partial class NewTravelPage : ContentPage
     {
-        public HomePage()
+        public NewTravelPage()
         {
             InitializeComponent();
-        }
-
-        private void MenuItem_OnClicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new NewTravelPage());
         }
     }
 }
